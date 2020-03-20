369fb6f81f5c8b1be6c75c481038bf98716673c5
Z <zijianhuang2000@gmail.com>
2020-03-20 21:58:25 +1000

    some example from Christian

---
+++ Core3WebApi/Controllers/StatisticsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core3WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class StatisticsController : ControllerBase
+    {
+        [HttpGet("distribution")]
+        public async Task<dynamic> GetDistribution()
+        {
+            return new
+            {
+                Id = 12345,
+                Name = "Something",
+            };
+
+        }
+    }
+}
