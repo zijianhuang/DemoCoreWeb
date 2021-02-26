6aa6b18cacf5bc7f9e5693150ab66eba1766eb7b
Z <zijianhuang2000@gmail.com>
2021-02-26 13:57:16 +1000

    refactoring

--- Tests/IntegrationTestsCore/HeroesApiIntegrationCore.cs
+++ Tests/IntegrationTestsCore/HeroesApiIntegrationCore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xunit;
+using System.Linq;
 
 
 namespace IntegrationTests
@@ -25,6 +26,38 @@ namespace IntegrationTests
 		}
 
 		DemoWebApi.Controllers.Client.Heroes api;
+
+		[Fact]
+		public void TestGet()
+		{
+			//var task = authorizedClient.GetStringAsync(new Uri(baseUri, "api/Heroes"));
+			//var text = task.Result;
+			//var array = JArray.Parse(text);
+			var array = api.Get().ToArray();
+			Assert.NotEmpty(array);
+		}
+
+		[Fact]
+		public void TestPost()
+		{
+			var hero = api.Post("Abc");
+			Assert.Equal("Abc", hero.Name);
+		}
+
+		[Fact]
+		public void TestPostWithQuery()
+		{
+			var hero = api.PostWithQuery("Xyz");
+			Assert.Equal("Xyz", hero.Name);
+		}
+
+		[Fact]
+		public void TestSearch()
+		{
+			var heroes = api.Search("Torna");
+			Assert.Single(heroes);
+			Assert.Equal("Tornado", heroes[0].Name);
+		}
 
 	}
 }
