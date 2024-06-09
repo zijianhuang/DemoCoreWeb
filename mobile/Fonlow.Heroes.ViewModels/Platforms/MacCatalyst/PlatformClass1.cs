403a0a8b4c91c6e0f96f4750ce95760171b4d277
zijianhuang <zijianhuang2000@gmail.com>
2024-06-09 15:34:57 +1000

    new MAUI

--- mobile/Fonlow.Heroes.ViewModels/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Fonlow.Heroes.ViewModelsMaui
-{
-	// All the code in this file is only included on Mac Catalyst.
-	public class PlatformClass1
-	{
-	}
-}
--- mobile/Fonlow.Heroes.ViewModels/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Fonlow.Heroes.ViewModelsMaui
-{
-	// All the code in this file is only included on iOS.
-	public class PlatformClass1
-	{
-	}
-}
