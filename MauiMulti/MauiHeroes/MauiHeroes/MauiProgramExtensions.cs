a6f1b7c65107f13d85fdcb97fa324d079027b935
zijianhuang <zijianhuang2000@gmail.com>
2024-06-09 16:02:34 +1000

    MAUI Multi

---
+++ MauiMulti/MauiHeroes/MauiHeroes/MauiProgramExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiHeroes
+{
+	public static class MauiProgramExtensions
+	{
+		public static MauiAppBuilder UseSharedMauiApp(this MauiAppBuilder builder)
+		{
+			builder
+				.UseMauiApp<Fonlow.Heroes.App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				});
+
+#if DEBUG
+			builder.Logging.AddDebug();
+#endif
+
+			return builder;
+		}
+	}
+}
