68ae8da21f22ce112fab4a6ee33c20f5ee548db8
zijianhuang <zijianhuang2000@gmail.com>
2024-06-11 16:09:46 +1000

    Blazor standalone OK

--- BlazorHeroesWAStandalone/Program.cs
+++ BlazorHeroesWAStandalone/Program.cs
@@ -7,5 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddHttpClient("Core3WebAPI", client =>client.BaseAddress = new Uri("http://localhost:5000"));
 await builder.Build().RunAsync();
