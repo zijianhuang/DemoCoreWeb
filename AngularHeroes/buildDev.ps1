f535abea7469bfd3d3c423922f7bd108dbb80ffa
Z <zijianhuang2000@gmail.com>
2026-03-11 18:01:21 +1000

    refactoring

--- AngularHeroes/buildDev.ps1
+++ AngularHeroes/buildDev.ps1
@@ -1,2 +1,12 @@
 ﻿Set-Location $PSScriptRoot
-ng build --configuration=dev
+$prod="dev"
+ng build --configuration=$prod
+
+copy-item "./src/conf/" -Destination "../ngdist/$prod/browser/conf/" -Force -Recurse
+$apiBaseUri='http://localhost:5000/'
+$siteConfigTemplate= Get-Content './src/conf_template/siteconfigTemplate.js' -Raw
+$updatedContent=$siteConfigTemplate -replace '\$apiBaseUri', $apiBaseUri
+Set-Content -Path "../ngdist/$prod/browser/conf/siteconfig.js" -Value $updatedContent
+
+Write-Output "apiBaseUri $apiBaseUri"
+Write-Output "done $(Get-Date)"
---
+++ AngularHeroes/src/conf_template/siteconfigTemplate.js
@@ -0,0 +1,13 @@
+const SITE_CONFIG = {
+	apiBaseUri: '$apiBaseUri',
+	themesDic: {
+		"assets/themes/azure-blue.css": { display: "Azure & Blue", dark: false },
+		"assets/themes/rose-red.css": { display: "Roes & Red", dark: false },
+		"assets/themes/magenta-violet.css": { display: "Magenta & Violet", dark: true },
+		"assets/themes/cyan-orange.css": { display: "Cyan & Orange", dark: true }
+	},
+	themeLoaderSettings: {
+		storageKey: 'app.theme',
+		themeLinkId: 'theme'
+	}
+}
