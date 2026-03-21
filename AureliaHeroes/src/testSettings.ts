a4cd59b8dc2cb5ecd58d8847bb718bd5d34119e2
Z <zijianhuang2000@gmail.com>
2026-03-21 16:16:24 +1000

    gone

--- AureliaHeroes/src/testSettings.ts
+++
@@ -1,9 +0,0 @@
-interface Site_Config {
-	apiBaseUri?: string;
-}
-
-declare const SITE_CONFIG: Site_Config
-
-export const SiteConfigConstants: Site_Config = {
-	...(typeof SITE_CONFIG === 'undefined' ? {} : SITE_CONFIG),
-}
--- AureliaHeroes/static/conf/siteconfig.js
+++
@@ -1,3 +0,0 @@
-const SITE_CONFIG = {
-	apiBaseUri: 'http://localhost:5000/',
-}
