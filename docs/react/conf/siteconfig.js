c4301cbb996ca642595ac783e95d99596c825b19
Z <zijianhuang2000@gmail.com>
2026-04-05 19:26:39 +1000

    published

--- docs/react/conf/siteconfig.js
+++ docs/react/conf/siteconfig.js
@@ -1,12 +1,15 @@
 const SITE_CONFIG = {
-	apiBaseUri: 'https://heroes.fonlow.net/webapi/',
+	apiBaseUri: 'https://heroes.fonlow.net/webapi/'
+}
+
+const THEME_CONFIG = {
 	themesDic: {
 		"assets/themes/light-theme.css": { display: "Light", dark: false },
 		"assets/themes/dark-theme.css": { display: "Dark", dark: true },
 		"assets/themes/pink-theme.css": { display: "Pink", dark: false }
 	},
 	themeLoaderSettings: {
-		storageKey: 'reactheroesapp.theme',
+		storageKey: 'app.theme',
 		themeLinkId: 'theme',
 		appColorsDir: 'conf/',
 		appColorsLinkId: 'app-colors',
